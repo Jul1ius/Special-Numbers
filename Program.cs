a07e35e6f2b311016f534654eebe37a7b224d364
its julii <nanji828@gmail.com>
2022-12-17 15:16:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int currentNum = i;
+                bool isSpecial = true;
+
+                while (currentNum > 0)
+                {
+                    int lastDigit = currentNum % 10;
+                    currentNum /= 10;
+                    if (lastDigit == 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+
+                    if (num % lastDigit != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+                if (isSpecial)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
